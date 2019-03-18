0688d2cb5ccfa408cd3aedc834be5d5b6e6dc9a0
wgrigor <44411193+wgrigor@users.noreply.github.com>
2019-03-18 14:34:56 -0500

    UID processing abstraction

--- wolfRFID.ino
+++ wolfRFID.ino
@@ -1,11 +1,57 @@
+#include <SPI.h>
+#include <MFRC522.h>
 #include <EEPROM.h>
+#include "wolfRfid.h"
+
+#define SS_PIN 10
+#define RST_PIN 9
+MFRC522 reader(SS_PIN, RST_PIN);
+
+byte cardData[8];
+byte eepromData[1024];
 
 void setup() {
   // put your setup code here, to run once:
-
+  Serial.begin(9600);
+  SPI.begin();
+  reader.PCD_Init();
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
 
+  if ( ! reader.PICC_IsNewCardPresent()) {
+    return;
+  }
+
+
+  if ( ! reader.PICC_ReadCardSerial()) {
+    return;
+  }
+  byte uidBytes[4];
+  readToArray(reader.uid.uidByte, uidBytes);
+  Serial.println(makeUidString(uidBytes));
+  reader.PICC_HaltA();
+
+}
+
+void getAllEeprom(byte eepromCells[]) {
+  for (int i = 0; i < 1024; i++) {
+    eepromCells[i] = EEPROM.read(i);
+  }
+}
+
+void readToArray(byte readerData[], byte output[]) {
+  for (int i = 0; i < 4; i++) {
+    output[i] = reader.uid.uidByte[i];
+  }
+}
+
+String makeUidString(byte in[]) {
+  String out;
+  char temp[2];
+  for (int i = 0; i < 4; i++) {
+    sprintf(temp, "%02x", in[i]);
+    out += temp;
+  }
+  return out;
 }
