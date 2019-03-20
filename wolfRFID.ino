ab2395b453c4efaf4df0abb2c10caeb9c218e1ad
wgrigor <44411193+wgrigor@users.noreply.github.com>
2019-03-20 12:28:56 -0500

    Read from EEPROM

--- wolfRFID.ino
+++ wolfRFID.ino
@@ -6,15 +6,18 @@
 #define SS_PIN 10
 #define RST_PIN 9
 MFRC522 reader(SS_PIN, RST_PIN);
-const byte arr[] = "cc08f034mm120p10d000cb70f0665rf099p12d081c";
-
+byte arr1[] = "533e9fd3mm120p10d000c969251d3rf099p12d081c";
+byte arr[43];
 byte cardData[8];
-byte eepromData[1025];
+//byte eepromData[1025];
 
 void setup() {
   Serial.begin(9600);
   SPI.begin();
   reader.PCD_Init();
+  //EEPROM.put(0, arr1);
+  //Serial.println("Wrote to eeprom");
+  EEPROM.get(0, arr);
 }
 
 void loop() {
@@ -28,17 +31,13 @@ void loop() {
     return;
   }
   String id = readID(reader);
-  //Serial.println(id);
+  Serial.println(id);
   String found = findInArray(arr, id);
+  //Serial.println(found);
+  //wolfData(found.c_str());
   Serial.println(wolfData(found.c_str()));
   reader.PICC_HaltA();
 
-}
-
-void getAllEeprom(byte eepromCells[]) {
-  for (int i = 0; i < 1024; i++) {
-    eepromCells[i] = EEPROM.read(i);
-  }
 }
 
 void readToArray(byte readerData[], byte output[]) {
@@ -64,8 +63,12 @@ String readID(MFRC522 _reader) {
   return makeUidString(uidBytes);
 }
 
-String wolfData(char wolfData[]) {
+String wolfData(String _wolfData) {
+  char wolfData[255];
+  _wolfData.toCharArray(wolfData, 22);
+  //Serial.println(_wolfData);
   String uid = sliceArray(wolfData, 0, 7);
+  //Serial.println(uid);
   char species_r = wolfData[8];
   String species;
   switch(species_r){
@@ -100,5 +103,6 @@ String wolfData(char wolfData[]) {
   String scan_count = sliceArray(wolfData, 17, 19);
   char data_out[255];
   sprintf(data_out, "UID: %s\nSpecies: %s\nSex: %s\nWeight: %slbs\nLast Seen: %s days\nScan Count: %s", uid.c_str(), species.c_str(), sex.c_str(), weight.c_str(), last_seen.c_str(), scan_count.c_str());
+  //Serial.println(data_out);
   return data_out;
 }
